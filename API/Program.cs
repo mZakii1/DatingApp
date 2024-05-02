452c078190f3b3f6d25de02bc4cb99f00d5c5012
mZakii1 <mzaki@egidegypt.com>
2024-05-02 14:43:55 +0300

    Initializa commit for angular

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
